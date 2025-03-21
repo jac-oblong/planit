f9546650d85024b914f3a66718826aaf34861cab
Jacob Long <jacoblong853@gmail.com>
2025-03-21 15:37:24 -0400

    [Meta] prettify license header

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////
+//                                                                        //
 // The MIT License (MIT)                                                  //
 //                                                                        //
 // Copyright (c) 2025 Jacob Long                                          //
@@ -21,6 +22,9 @@
 // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   //
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      //
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 //
+//                                                                        //
 ////////////////////////////////////////////////////////////////////////////
 
+pub mod app;
 pub mod core;
+pub mod ui;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////
+//                                                                        //
 // The MIT License (MIT)                                                  //
 //                                                                        //
 // Copyright (c) 2025 Jacob Long                                          //
@@ -21,6 +22,7 @@
 // CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   //
 // TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      //
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 //
+//                                                                        //
 ////////////////////////////////////////////////////////////////////////////
 
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
